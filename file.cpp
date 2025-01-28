d78e4081c62f2b2f25a03c9cd23cde94ebb7adc9
vikaskamblenitp <vikasmkamble007@gmail.com>
2025-01-28 06:23:24 +0530

    chore: dsa setup

--- file.cpp
+++ file.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 
 int main(){
-	int n;
-	cin>>n;
-	bool val =false;
-	cout<<val<<endl;
-	cout<<(++(n*2))<<endl;
-	// while(n--){
-	// 	cout<<n;
-	// }
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		vector<int> v(n);
+		for(int i=0;i<n;i++){
+			cin>>v[i];
+			v.push_back(v[i]+1);
+			cout<<__builtin_popcount(v[i])<<" ";	
+		}
+		cout<<"\n";
+		for(auto num : v) cout<<num<<" ";
+		cout<<"\n"<<*max_element(v.begin(), v.end())<<" "<<*min_element(v.begin(), v.end());	
+	}
+	
+
 	return 0;
 }
